82390ad4c1f1f5feaa332f8e4d2cf63a58df4458
Csabi1998 <61312438+Csabi1998@users.noreply.github.com>
2021-05-15 11:45:21 +0200

    Izolacio

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,9 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500( TorpedoStore first, TorpedoStore second) {
+    this.primaryTorpedoStore = first;
+    this.secondaryTorpedoStore = second;
   }
 
   public boolean fireLaser(FiringMode firingMode) {
@@ -79,12 +79,23 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores branch B and A
         //TODO implement feature
-        if(!secondaryTorpedoStore.isEmpty() && !primaryTorpedoStore.isEmpty()){
+        //if(!secondaryTorpedoStore.isEmpty() && !primaryTorpedoStore.isEmpty()){
+        //  firingSuccess = primaryTorpedoStore.fire(1);
+        //  boolean next = secondaryTorpedoStore.fire(1);
+        //  if(!(firingSuccess && next)){
+        //    firingSuccess = false;
+        //  }
+        //}
+        boolean secondFire = false;
+        while(!primaryTorpedoStore.isEmpty() && !firingSuccess){
           firingSuccess = primaryTorpedoStore.fire(1);
-          boolean next = firingSuccess = secondaryTorpedoStore.fire(1);
-          if(!(firingSuccess && next)){
-            firingSuccess = false;
-          }
+        }
+        while(!secondaryTorpedoStore.isEmpty() && !secondFire){
+          secondFire = secondaryTorpedoStore.fire(1);
+        }
+
+        if(!secondFire){
+          firingSuccess = false;
         }
         break;
     }
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,42 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockedTorpedoFirst;
+  private TorpedoStore mockedTorpedoSecond;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockedTorpedoFirst = mock(TorpedoStore.class);
+    mockedTorpedoSecond = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockedTorpedoFirst, mockedTorpedoSecond);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockedTorpedoFirst.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(mockedTorpedoFirst, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(mockedTorpedoFirst.fire(1)).thenReturn(true);
+    when(mockedTorpedoSecond.fire(1)).thenReturn(true);
+    when(mockedTorpedoFirst.isEmpty()).thenReturn(false, true);
+    when(mockedTorpedoSecond.isEmpty()).thenReturn(false, true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(mockedTorpedoFirst, times(1)).fire(1);
+    verify(mockedTorpedoSecond, times(1)).fire(1);
   }
 
 }
